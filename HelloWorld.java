61e6afcefd4a7fc2d6b396cd3be4d4e49da89e95
Vini-ceno <138730290+Vini-ceno@users.noreply.github.com>
2024-09-23 21:31:32 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,47 @@
+/**
+ * Public: declaração de exportação pública de uma classe/método/função/váriavel/etc...
+ * 
+ * Class: declaração de classe (objeto)
+ * 
+ * HelloWorld: nome da classe (principal), que tem que ser igual o nome do arquivo.java
+ * 
+ * { "abre chaves": inicia o bloco de código
+ * 
+ * static: Declaração de método/função/variável estática, pois essa declaração faz com que os dados não sejam perdido/vazados na memória, ou processamento
+ * 
+ * Void (nulo): nesse caso, sgnifica que o método não irar retornar, ou seja o retorno é "nulo"
+ * 
+ * main: nome do método, neste caso, é o método principal que irá executar o projeto;
+ * 
+ * ( "abre parênteses: inicio da declaração de parâmetros ou argumentos"
+ * 
+ * string (texto): é o tipo do parâmetro ou argumento, serve também para declarar tipo de váriavel
+ * 
+ * [] "abre colchete": Declaração de uma matriz
+ * 
+ * args (argumetnos): é o nome do parãmetro ou argumetno
+ * 
+ * ) "fechar parênteses": fim da declaração de parâmetros ou argumentos
+ * 
+ * System (sistema): declaração/invocação da classe "System"
+ * 
+ * . "ponto": Operador de invocação
+ * 
+ * out (saída): depois do ponto, serve como uma declaração/invocação da subclasse "out" proveninente da classe "System"
+ *  
+ * Println (imprimir linha): depois do ponto serve como uma declaração/incovação do método "println" pois logo após tem um "abre parêntes" 
+ * Obs: Quando tem o "new" antes é uma declaração/incovação/criação de um novo objeto
+ * 
+ * Hello World! :) : é o texto propriamente dito, dentro de aspas duplas
+ * 
+ * "": aspas duplas: declaração/definição de texto
+ * 
+ * ; "ponto e virgula": é a declaração de finalização de linha de código
+ * 
+ * } Fecha chaves: é a declaração de finalização de bloco de código
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! :)");
+    }
+}
